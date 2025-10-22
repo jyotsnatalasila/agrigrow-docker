8e253c3b463664cc64489caac8fc0eef8c69563e
jyotsnatalasila <jyotsnatalasila@gmail.com>
2025-10-22 21:40:35 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
   return (
     <LocationProvider> 
       <Router>
-        <Routes basename="/agrigrowfe">
+        <Routes basename="/agrigrowfe/">
           {/* Public Routes */}
           <Route path="/" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
